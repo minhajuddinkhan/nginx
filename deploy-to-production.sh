9d2328677eaed85ab8ff5ab2b7cdd191e517ff09
Minhaj <minhajuddinkhan75@gmail.com>
2017-08-26 16:03:32 +0500

    ansible installation added

--- deploy-to-production.sh
+++ deploy-to-production.sh
@@ -1,3 +1,13 @@
 #/bin/bash
 
+if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") = 0 ]
+then
+    sudo apt-get update -y && \
+    sudo apt-get install software-properties-common -y && \
+    sudo apt-add-repository ppa:ansible/ansible -y && \
+    sudo apt-get update -y && \
+    sudo apt-get install ansible -y
+fi
+
+
 sudo ansible-playbook -v playbooks/setup.yml
